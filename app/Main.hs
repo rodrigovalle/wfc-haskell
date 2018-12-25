236a5606f6de1415b64cd466c1e4f40917137912
Rodrigo Valle <rdan.valle@gmail.com>
2018-12-24 23:07:53 -0800

    Run hindent on source files

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,15 @@
 module Main where
 
-import System.Environment
 import Lib
+import System.Environment
 
 usage = error "Usage: wfc-haskell-exe inputbmp outputbmp"
 
 main :: IO ()
-main = do args <- getArgs
-          case args of [] -> usage
-                       [_] -> usage
-                       [inputFile, outputFile] -> return () -- call into Lib.hs
-                       _ -> usage
+main = do
+    args <- getArgs
+    case args of
+        [] -> usage
+        [_] -> usage
+        [inputFile, outputFile] -> return () -- call into Lib.hs
+        _ -> usage
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,28 +2,29 @@ module Lib
     ( loadBitmap,
     ) where
 
+import Codec.BMP
 import qualified Data.ByteString as BS
 import qualified Data.Counter as Counter
-import Codec.BMP
 import Data.Word
 
 type Pixel = (Word8, Word8, Word8)
 type Bitmap = [[Pixel]] -- better performance with repa arrays?
 
 loadBitmap :: String -> IO Bitmap
-loadBitmap filename = do Right bmp <- readBMP filename
-                         let rgba = BS.unpack $ unpackBMPToRGBA32 bmp
-                             (width, height) = bmpDimensions bmp
-                             bitmap = constructBitmap width rgba
-                         return bitmap
+loadBitmap filename = do
+    Right bmp <- readBMP filename
+    let rgba = BS.unpack $ unpackBMPToRGBA32 bmp
+        (width, height) = bmpDimensions bmp
+        bitmap = constructBitmap width rgba
+    return bitmap
 
 -- Use a list comprehension instead?
 -- Make it pointfree?
 constructBitmap :: Int -> [Word8] -> Bitmap
 constructBitmap width [] = []
 constructBitmap width xs = go head : constructBitmap width tail
-    where
-          go [] = []
-          go (r:g:b:_a:xs) = (r,g,b) : go xs
-          go _ = error "corrupt BMP"
-          (head, tail) = splitAt (width*4) xs
+  where
+    go [] = []
+    go (r:g:b:_a:xs) = (r, g, b) : go xs
+    go _ = error "corrupt BMP"
+    (head, tail) = splitAt (width * 4) xs
