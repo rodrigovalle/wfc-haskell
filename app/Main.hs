08ae50e922395d7fd7ac06f5b89c97845070bace
Rodrigo Valle <rdan.valle@gmail.com>
2018-12-24 22:50:31 -0800

    Add functions for loading bitmaps

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
+import System.Environment
 import Lib
 
+usage = error "Usage: wfc-haskell-exe inputbmp outputbmp"
+
 main :: IO ()
-main = someFunc
+main = do args <- getArgs
+          case args of [] -> usage
+                       [_] -> usage
+                       [inputFile, outputFile] -> return () -- call into Lib.hs
+                       _ -> usage
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,6 @@
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do bitmap <- loadBitmap "Dungeon.bmp"
+          print bitmap
+          return ()
